afdeca500e3a18a08ce134a8e006355c9337b482
alanasp87-creator <alanasp87@gmail.com>
2026-03-31 20:03:02 +0100

    Add js/fuel-card-networks.js

---
+++ js/fuel-card-networks.js
@@ -0,0 +1,41 @@
+/**
+ * UK / Ireland fuel card scheme names for add-fuel-card dropdowns.
+ */
+(function () {
+  "use strict";
+
+  window.FUEL_CARD_NETWORK_OPTIONS = [
+    "Allstar",
+    "UK Fuels",
+    "Keyfuels",
+    "BP Plus",
+    "BP Bunker",
+    "Shell Fleet",
+    "Shell CRT",
+    "Esso National",
+    "Esso Card Europe",
+    "Texaco Fastfuel",
+    "JET Fuel Card",
+    "Gulf Fuel Card",
+    "Gleaner",
+    "fuelGenie",
+    "The Right Fuel Card Company",
+    "Fleetmaxx Solutions",
+    "Fuel Card Services",
+    "InterFuels",
+    "The Fuel Store",
+    "Fuelmate",
+    "Fuelwise",
+    "Diesel Card Ireland (DCI)",
+    "Truckone",
+    "Greenarc",
+    "Silvey Fleet",
+    "WEX Europe Services",
+    "Rally",
+    "Mina",
+    "Rightcharge",
+    "Paua",
+    "DKV",
+    "EDC (European Diesel Card)",
+  ];
+})();
